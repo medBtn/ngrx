f2894978153781740caea31e5f76bf7b9bed1e4a
medBtn <boutine111@gmail.com>
2026-02-24 14:22:26 +0100

    feat(toast): integrate angualr popup package.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideEffects } from '@ngrx/effects';
 import { provideState, provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
 
+import { provideNgToast } from 'ng-angular-popup';
 import * as authEffect from './shared/store/auth-effect';
 import { authFeature } from './shared/store/auth-feature';
 
@@ -20,6 +21,15 @@ export const appConfig: ApplicationConfig = {
     {
       provide: API_URL,
       useValue: 'https://fakestoreapi.com'
-    }
+    },
+    provideNgToast({
+      duration: 2000,              // Default 5 seconds
+      position: 'toaster-top-right',       // Default position
+      width: 400,                  // Toast width in pixels
+      showProgress: true,          // Show progress bar
+      dismissible: true,           // Allow manual dismiss
+      showIcon: true,              // Show icons
+      enableAnimations: true       // Enable animations
+    }),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgToastComponent } from "ng-angular-popup";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgToastComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
