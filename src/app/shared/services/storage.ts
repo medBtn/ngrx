aa79e988fc708bf9473cdeb8bd4403275ed4f342
medBtn <boutine111@gmail.com>
2026-02-24 12:37:05 +0100

    feat(auth):create storage service.

---
+++ src/app/shared/services/storage.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Storage {
+
+    setItem(key: string, value: string) {
+        localStorage.setItem(key, value);
+    }
+
+    getItem(key: string) {
+        return localStorage.getItem(key);
+    }
+
+    removeItem(key: string) {
+        localStorage.removeItem(key);
+    }
+    clear() {
+        localStorage.clear();
+    }
+}
