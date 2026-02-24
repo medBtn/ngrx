8b691c645fa765ca2eb9552aa22ad888096d8076
medBtn <boutine111@gmail.com>
2026-02-24 11:34:20 +0100

    feat(auth): create auth features login & register.

---
+++ src/app/shared/store/auth-feature.ts
@@ -0,0 +1,58 @@
+import { createFeature, createReducer, on } from "@ngrx/store";
+import { authActions } from "./auth-actions";
+
+export type AuthState = {
+    token: string | null;
+    userId: number | null;
+    error: string | null;
+    isLoading: boolean;
+}
+
+export const initialState: AuthState = {
+    token: null,
+    userId: null,
+    error: null,
+    isLoading: false
+}
+
+export const authFeature = createFeature({
+    name: 'auth',
+    reducer: createReducer(
+        initialState,
+
+        // Login actions
+        on(authActions.loginSuccess, (state, { token }) => ({
+            ...state,
+            token
+        })),
+
+        on(authActions.loginFailure, (state, { error }) => ({
+            ...state,
+            error
+        })),
+
+        on(authActions.login, (state) => ({
+            ...state,
+            isLoading: true,
+            error: null
+        })),
+
+        // Register actions
+        on(authActions.registerSuccess, (state) => ({
+            ...state,
+            isLoading: false
+        })),
+
+        on(authActions.registerFailure, (state, { error }) => ({
+            ...state,
+            isLoading: false,
+            error
+        })),
+
+        on(authActions.register, (state) => ({
+            ...state,
+            isLoading: true,
+            error: null
+        }))
+    )
+})
