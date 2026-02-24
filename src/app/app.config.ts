1e3da4e8fe0522c35c2b474a3c38ff0e7f7efd86
medBtn <boutine111@gmail.com>
2026-02-24 10:30:47 +0100

    feat(auth-api): create auth api service and connect it wirh fakestoreapi.com. -Add ngrx store config.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
+
+export const API_URL = new InjectionToken<string>('API_URL');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore(),
+    {
+      provide: API_URL,
+      useValue: 'https://fakestoreapi.com'
+    }
   ]
 };
---
+++ src/app/shared/services/auth-api.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { API_URL } from "../../app.config";
+
+
+export type LoginRequest = {
+    username: string;
+    password: string;
+}
+
+export type LoginResponse = {
+    token: string;
+}
+
+export type RegisterRequest = {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthApi {
+    private readonly baseApiUrl = inject(API_URL);
+    private readonly http = inject(HttpClient);
+
+    login(request: LoginRequest): Observable<LoginResponse> {
+        return this.http.post<LoginResponse>(`${this.baseApiUrl}/auth/login`, request);
+    }
+
+    register(request: RegisterRequest): Observable<RegisterRequest> {
+        return this.http.post<RegisterRequest>(`${this.baseApiUrl}/auth/register`, request);
+    }
+
+}
