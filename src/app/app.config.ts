6ce35301b2a30ff76d405a492b9b46894ec82820
medBtn <boutine111@gmail.com>
2026-02-24 11:36:10 +0100

    feat(auth): provide effects and state for auth config.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
 import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
+
+import * as authEffect from './shared/store/auth-effect';
+import { authFeature } from './shared/store/auth-feature';
 
 export const API_URL = new InjectionToken<string>('API_URL');
 
@@ -11,6 +15,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideStore(),
+    provideEffects(authEffect),
+    provideState(authFeature),
     {
       provide: API_URL,
       useValue: 'https://fakestoreapi.com'
